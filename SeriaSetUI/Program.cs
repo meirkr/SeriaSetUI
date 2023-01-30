e8c5dc2c13a9a4bcfdce21eca44a07459850cd0b
meir kriheli <meirkr@gmail.com>
2023-01-30 22:05:03 +0200

    from new .net 7 template

--- SeriaSetUI/Program.cs
+++ SeriaSetUI/Program.cs
@@ -1,13 +1,11 @@
-using System;
-using System.Net.Http;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-
+using SeriaSetUI;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<SeriaSetUI.App>("app");
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
-
